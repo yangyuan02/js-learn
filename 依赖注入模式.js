fe0f6f96d0086dae1f4f25722cd56922fac83ea0
yangyuan <1367511704@qq.com>
2019-01-03 11:29:44 +0800

    feat(依赖注入)

---
+++ 依赖注入模式.js
@@ -0,0 +1,44 @@
+var DI = function(dependency) {
+  this.dependency = dependency;
+};
+
+// Should return new function with resolved dependencies
+DI.prototype.inject = function(func) {
+  // Your code goes here
+};
+
+// 要注入的依赖
+var deps = {
+  dep1: function() {
+    return "this is dep1";
+  },
+  dep2: function() {
+    return "this is dep2";
+  },
+  dep3: function() {
+    return "this is dep3";
+  },
+  dep4: function() {
+    return "this is dep4";
+  }
+};
+
+// 新建一个“注射器”
+var di = new DI(deps);
+
+// 注射
+var myFunc = di.inject(function(dep3, dep1, dep2) {
+  return [dep1(), dep2(), dep3()].join(" -> ");
+});
+
+
+var services = {
+    A: () => {console.log(1)},
+    B: () => {console.log(2)},
+    C: () => {console.log(3)},
+};
+
+function Service(A, B) {
+    A();
+    B();
+}
