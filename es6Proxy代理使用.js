2f863671c44cced7f6665531ae1050816a95190b
yangyuan <1367511704@qq.com>
2019-01-21 13:47:14 +0800

    proxy的使用

--- es6Proxy代理使用.js
+++ es6Proxy代理使用.js
@@ -62,5 +62,63 @@ proxy.count++
 console.log(proxy.count)
 
 
+// new Proxy构建表单校验器
+
+function validator(target, validator, errorMsg) {
+    return new Proxy(target, {
+        _validator: validator,
+        set(target, key, value, proxy) {
+            let errMsg = errorMsg;
+            if (value) {
+                alert(`${errMsg[key]}不能为空`)
+                return target[key] = false;
+            }
+            let va = this._validator[key]
+            if (!!va(value)) {
+                return Reflect.set(target, key, value, proxy);
+            } else {
+                alert(`${errMsg[key]}格式不正确`)
+                return target[key] = false;
+            }
+        }
+    })
+}
+
+// 负责校验的逻辑代码
+
+const validators = {
+    name(value) {
+        return value.length > 6;
+    },
+    password(value) {
+        return value.length > 6;
+    },
+    moblie(value) {
+        return /^1(3|5|7|8|9)[0-9]$/.test(value)
+    },
+    email(value) {
+        return /^\w+([+-.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(value)
+    }
+}
+
+// 调用逻辑
+
+const errorMsg = {name: '用户名', passwd: '密码', moblie: '手机号码', email: '邮箱地址'}
+const vali = validator({}, validators, errorMsg);
+let registerForm = document.querySelector("#registerForm");
+registerForm.addEventListener('submit', function(){
+    let validatorNext = function*() {
+        yield vali.name = registerForm.userName.value;
+        yield vali.passwd = registerForm.passWord.value;
+        yield vali.moblie = registerForm.phoneNumber.value;
+        yield vali.email = registerForm.emailAddress.value;
+    }
+    let validator = validatorNext();
+    validator.next();
+    !vali.name||validator.next() //上一步的检验通过才执行下一步
+    !vali.passwd||validator.next() //上一步的检验通过才执行下一步
+    !vali.email||validator.next() //上一步的检验通过才执行下一步
+})
+
 
 
