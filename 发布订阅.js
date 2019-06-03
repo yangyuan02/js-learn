a9b38f1d6ab5c39a55bd67e552de307c59d990b7
yangyuan <1367511704@qq.com>
2019-06-03 15:09:27 +0800

    fix(发布订阅)

---
+++ 发布订阅.js
@@ -0,0 +1,55 @@
+// 发布订阅
+class EventEmitter {
+  constructor() {
+    this._event = Object.create(null);
+  }
+  on(type, handler) {
+    (this._events[type] || (this._events[type] = [])).push(handler);
+  }
+  off(type, handler) {
+    if (this._events[type]) {
+      this._events[type].splice(this._events[type].indexOf(handler) >>> 0, 1);
+    }
+  }
+  once(type, handler) {
+    let fired = false;
+    this.off(type, magic);
+    if (!fired) {
+      fired = true;
+      handler.apply(this, arguments);
+    }
+    this.on(type, magic);
+  }
+  emit(type) {
+    let payload = [].slice.call(arguments, 1);
+    let array = this._events[type] || [];
+    for (let i = 0; i < array.length; i++) {
+      let handler = this._events[type][i];
+      handler.apply(this, payload);
+    }
+  }
+}
+export default EventEmitter;
+
+// 观察者
+class Subject {
+  constructor() {
+    this.observer_list = [];
+  }
+  add_observer(obj) {
+    this.add_observer.push(obj);
+  }
+  remove_boserver(obj) {
+    for (let i = 0; i < this.observer_list.length; i++) {
+      if (this.observer_list[i] === obj) {
+        this.observer_list.splice(i, 1);
+      }
+    }
+  }
+  notify() {
+    let args = Array.prototype.slice.call(arguments, 0);
+    for (let i = 0; i < this.observer_list.length; i++) {
+      this.observer_list.update(args);
+    }
+  }
+}
