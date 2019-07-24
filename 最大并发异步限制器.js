4fbe92951cbe20276f67d984b2760071cf5615d7
yangyuan <1367511704@qq.com>
2019-07-24 17:38:55 +0800

    fix(调度器bug)

--- 最大并发异步限制器.js
+++ 最大并发异步限制器.js
@@ -28,3 +28,17 @@ class Scheduler {
     }
   }
 }
+const timeout = time =>
+  new Promise(resolve => {
+    setTimeout(resolve, time);
+  });
+
+const scheduler = new Scheduler();
+const addTask = (time, order) => {
+  scheduler.add(() => timeout(time)).then(() => console.log(order));
+};
+
+addTask(1000, '1');
+addTask(500, '2');
+addTask(300, '3');
+addTask(400, '4');
