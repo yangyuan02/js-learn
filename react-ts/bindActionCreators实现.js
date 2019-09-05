f496565c770ae4a49d0f3799ef5db2c087edaf3c
yangyuan <1367511704@qq.com>
2019-09-05 11:44:22 +0800

    feat(bindActionCreator实现)

--- react-ts/bindActionCreators实现.js
+++ react-ts/bindActionCreators实现.js
@@ -2,7 +2,7 @@
  * @Author: yangyuan
  * @Date: 2019-09-05 11:34:01
  * @Email: 1367511704@qq.com
- * @LastEditTime: 2019-09-05 11:34:53
+ * @LastEditTime: 2019-09-05 11:43:33
  * @Description:
  */
 connect(
