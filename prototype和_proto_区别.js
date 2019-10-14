a135881c6a0b2cc81666def3f028871279bb2e7f
yangyuan <1367511704@qq.com>
2019-10-14 15:28:42 +0800

    fix(关于prototype和_proto_区别)

---
+++ prototype和_proto_区别.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: yangyuan
+ * @Date: 2019-10-14 15:09:41
+ * @Email: 1367511704@qq.com
+ * @LastEditTime: 2019-10-14 15:28:00
+ * @Description:
+ */
+
+// _proto_是每个实例都有的属性
+// prototype是构造函数的属性
