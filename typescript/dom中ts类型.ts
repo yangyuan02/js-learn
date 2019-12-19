3e71fc5bfb1fabe8692edf7627520f21d518e1ce
yangyuan <1367511704@qq.com>
2019-12-19 17:47:25 +0800

    fix(dom中ts的一些写法)

--- typescript/dom中ts类型.ts
+++ typescript/dom中ts类型.ts
@@ -5,16 +5,20 @@
  * @LastEditTime : 2019-12-19 16:40:15
  * @Description:
  */
-// HTMLElementTagNameMap中查看有哪些集合
+// HTMLElementTagNameMap 中查看有哪些集合
 
+// createElement
 const div: HTMLDivElement = document.createElement('div');
 
 const span: HTMLSpanElement = document.createElement('span');
 
 const p: HTMLParagraphElement = document.createElement('p');
 
-const a: HTMLAnchorElement = document.createElement('a');
-
+// getElementById
 const divEle: HTMLElement = document.getElementById('div');
 
+// querySelector
 const spanEle = document.querySelector<HTMLSpanElement>('span');
+
+// querySelectorAll
+const spanEleAll = document.querySelectorAll<HTMLSpanElement>('span');
