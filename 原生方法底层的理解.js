9e86fcb502c0e650a99ece8d28f4f3696ba9a423
yangyuan <1367511704@qq.com>
2020-01-16 15:40:50 +0800

    fix(一些修改)

--- 原生方法底层的理解.js
+++ 原生方法底层的理解.js
@@ -2,7 +2,7 @@
  * @Author: yangyuan
  * @Date: 2020-01-16 10:54:56
  * @Email: 1367511704@qq.com
- * @LastEditTime : 2020-01-16 10:57:10
+ * @LastEditTime : 2020-01-16 11:15:02
  * @Description: 
  */
 indexOf() // 内部使用的是 ===
@@ -18,4 +18,7 @@ undefined === undefined // true
 [1,2,NaN, {}].indexOf(NaN) // -1
 
 [1,2, NaN, {}].indexOf({}) // -1
+
+var arr [1,1,'true','true',true,true,15,15,false,false, undefined,undefined, null,null, NaN, NaN,'NaN', 0, 0, 'a', 'a',{},{}];
+// 去重的时候最好用 includes() includes(NaN) // true
 
