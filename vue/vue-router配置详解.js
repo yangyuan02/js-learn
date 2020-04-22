9be061e199cc0924017e08769b606ba542ade089
yangyuan <1367511704@qq.com>
2020-04-22 14:40:47 +0800

    fix(vue-router使用笔记)

--- vue/vue-router配置详解.js
+++ vue/vue-router配置详解.js
@@ -2,7 +2,7 @@
  * @Author: yangyuan
  * @Date: 2020-04-17 11:27:19
  * @Email: 1367511704@qq.com
- * @LastEditTime: 2020-04-17 17:58:48
+ * @LastEditTime: 2020-04-22 14:17:27
  * @Description:
  */
 import Vue from "vue";
@@ -43,3 +43,20 @@ afterEach((to, from) => {
 // beforeRouteUpdate 再本路由的下级路由才会切换出发beforeRouteUpdate
 
 // 动态路由、嵌套路由、未完待续
+
+/* vue-router 传参
+    字符串
+        this.$router.push("home") // home 指的是组件name属性,方式很简单,但无法传递参数
+    对象
+        想要传递参数主要就是以对象来写,分为两种方式:命名路由,查询参数,下面说下注意实现
+        {
+            path:'/news',
+            name:'news',
+            component:News
+        }
+        组件name命令路由传递参数需要使用params来传递,这里一定要注意使用params不是query,目标页面接受传递参数时使用params
+            跳转:this.$router.push({name:'news',params:{id:123}})
+            接受:this.$route.params.id
+        
+
+*/
