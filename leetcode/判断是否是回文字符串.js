79eacca1c25396f7b36785df13dfd505c431b091
yangyuan <1367511704@qq.com>
2020-04-28 10:35:10 +0800

    fix(判断是否是回文字符串)

---
+++ leetcode/判断是否是回文字符串.js
@@ -0,0 +1,28 @@
+/*
+ * @Author: yangyuan
+ * @Date: 2020-04-28 10:13:36
+ * @Email: 1367511704@qq.com
+ * @LastEditTime: 2020-04-28 10:19:47
+ * @Description:
+ */
+
+//  回文字符串 noon level 就是正读反读都是一个意思
+
+function isPlalindrome() {
+    if (typeof input !== "string") return false;
+    return input.split("").reverse().join("") === input;
+}
+
+// 不适用api
+
+function isPlalindrome() {
+    if (typeof input !== "string") return false;
+    let i = 0,
+        j = input.length - 1;
+    while (i < j) {
+        if (input.charAt(i) !== input.charAt(j)) return false;
+        i++;
+        j--;
+    }
+    return true;
+}
