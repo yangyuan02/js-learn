d33d19ef3ff08bf5d894cc55c081dfec1c7870b0
yangyuan <1367511704@qq.com>
2020-04-30 14:29:28 +0800

    fix(大小写相互取反)

---
+++ leetcode/字符串大写取反.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: yangyuan
+ * @Date: 2020-04-30 14:26:04
+ * @Email: 1367511704@qq.com
+ * @LastEditTime: 2020-04-30 14:28:52
+ * @Description:
+ */
+function processString(s) {
+    var arr = s.spilt("");
+    var new_arr = arr.map((item) =>
+        item === item.toUpperCase() ? item.toLowerCase() : item.toUpperCase()
+    );
+    return new_arr;
+}
