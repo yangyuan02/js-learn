53863262d638f1511329b469a18f446a63c96cda
yangyuan <1367511704@qq.com>
2020-04-30 14:30:58 +0800

    fix(大小写相互取反)

--- leetcode/字符串大写取反.js
+++ leetcode/字符串大写取反.js
@@ -2,7 +2,7 @@
  * @Author: yangyuan
  * @Date: 2020-04-30 14:26:04
  * @Email: 1367511704@qq.com
- * @LastEditTime: 2020-04-30 14:28:52
+ * @LastEditTime: 2020-04-30 14:30:26
  * @Description:
  */
 function processString(s) {
@@ -10,5 +10,5 @@ function processString(s) {
     var new_arr = arr.map((item) =>
         item === item.toUpperCase() ? item.toLowerCase() : item.toUpperCase()
     );
-    return new_arr;
+    return new_arr.join("");
 }
