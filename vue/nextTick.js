ddeb27f0fb0230f8d20519cc0e28f9d7c186a20e
yangyuan <1367511704@qq.com>
2020-05-14 15:32:10 +0800

    fix(react笔记)

--- vue/nextTick.js
+++ vue/nextTick.js
@@ -2,7 +2,7 @@
  * @Author: yangyuan
  * @Date: 2019-07-25 14:30:26
  * @Email: 1367511704@qq.com
- * @LastEditTime: 2019-08-29 14:40:05
+ * @LastEditTime: 2020-05-14 14:48:24
  */
 //juejin.im/post/5c03500e6fb9a049d37ed754
 https: Vue源码解析之nextTick;
@@ -11,18 +11,20 @@ let callbacks = [];
 let pending = false;
 
 function nextTick(cb) {
-  callbacks.push(cb);
-  if (!pending) {
-    pending = true;
-    setTimeout(flushCallback, 0);
-  }
+    callbacks.push(cb);
+    if (!pending) {
+        pending = true;
+        setTimeout(flushCallback, 0);
+    }
 }
 
 function flushCallback() {
-  pending = false;
-  let copies = callbacks.slice();
-  callbacks.length = 0;
-  copies.forEach(copy => {
-    copy();
-  });
+    pending = false;
+    let copies = callbacks.slice();
+    callbacks.length = 0;
+    copies.forEach((copy) => {
+        copy();
+    });
 }
+
+// nexttick在vue里的执行机制，对应事件循环的哪一步
