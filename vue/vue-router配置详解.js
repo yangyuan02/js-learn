db5883ba83a7ce717b3d520c809360956f8d1e57
yangyuan <1367511704@qq.com>
2020-05-21 16:18:07 +0800

    fix(和区别)

--- vue/vue-router配置详解.js
+++ vue/vue-router配置详解.js
@@ -2,7 +2,7 @@
  * @Author: yangyuan
  * @Date: 2020-04-17 11:27:19
  * @Email: 1367511704@qq.com
- * @LastEditTime: 2020-05-18 14:24:51
+ * @LastEditTime: 2020-05-21 16:17:04
  * @Description:
  */
 import Vue from "vue";
@@ -134,3 +134,10 @@ router.afterEach(to => {
 // next()
 // next(false) //禁止跳转
 // next(path) // 需要跳转到的路径
+
+// $router $route区别
+// $router是VueRouter的一个对象，通过Vue.use(VueRouter)和Vu构造函数得到一个router的实例对象，
+// 这个对象中是一个全局的对象，他包含了所有的路由，包含了许多关键的对象和属性
+
+// $route是一个跳转的路由对象，每一个路由都会有一个$route对象，是一个局部的对象，
+// 可以获取对应的name，path，params，query等
