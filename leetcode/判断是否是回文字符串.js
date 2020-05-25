32ea917067bc6a9ff3e0a1e7d9dee57cde19baf5
yangyuan <1367511704@qq.com>
2020-05-25 17:30:28 +0800

    fix(更新判断是否是回文)

--- leetcode/判断是否是回文字符串.js
+++ leetcode/判断是否是回文字符串.js
@@ -2,20 +2,20 @@
  * @Author: yangyuan
  * @Date: 2020-04-28 10:13:36
  * @Email: 1367511704@qq.com
- * @LastEditTime: 2020-04-28 10:19:47
+ * @LastEditTime: 2020-05-25 17:30:01
  * @Description:
  */
 
 //  回文字符串 noon level 就是正读反读都是一个意思
 
-function isPlalindrome() {
+function isPlalindrome(input) {
     if (typeof input !== "string") return false;
     return input.split("").reverse().join("") === input;
 }
 
 // 不适用api
 
-function isPlalindrome() {
+function isPlalindrome(input) {
     if (typeof input !== "string") return false;
     let i = 0,
         j = input.length - 1;
