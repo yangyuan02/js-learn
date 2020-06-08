7cfe8218bf3efa6e8ff537de2f17b2228826f398
yangyuan <1367511704@qq.com>
2020-06-08 16:34:30 +0800

    fix(更新格式化)

--- vue/search-component/index.vue
+++ vue/search-component/index.vue
@@ -1,9 +1,4 @@
-<!--
- * @Author: yangyuan
- * @Date: 2020-06-02 10:52:12
- * @Email: 1367511704@qq.com
- * @LastEditTime: 2020-06-02 10:52:13
- * @Description: <script>
+<script>
 /**
   使用说明：
   属性            类型          作用                                                 案例
@@ -45,109 +40,167 @@
 </script>
 
 <template>
-  <div class="sd-search-form">
-    <el-form ref="form" :model="formModel" :inline="false" :label-width="formLayouts.labelWidth">
-      <el-row :gutter="formLayouts.gutter">
-        <el-col :sm="12" :md="8" :lg="6"
-          v-for="(formItem, index) in formItems" 
-          :key="formItem.key || index">
-
-          <transition name="el-zoom-in-top">
-            <el-form-item
-              :label="formItem.label"
-              :prop="formItem['model']"
-              v-show="!formItem.hidden">
-            
-            <!-- type === input-->
-            <el-col :span="formItem.colSpan || defaultColSpan" v-if="formItem.controlName === 'input'">
-              <el-input
-                  v-model="formModel[formItem['model']]"
-                  :placeholder="formItem.placeholder"
-                  :clearable="formItem.clearable"
-                  :type="formItem.type || 'text'"
-                  @change="controlChange(formItem['model'])">
-              </el-input>
-            </el-col>
-          
-            <!-- type === select -->
-            <el-col :span="formItem.colSpan || defaultColSpan" v-else-if="formItem.controlName === 'select'">
-              <el-select
-                v-model="formModel[formItem['model']]"
-                :placeholder="formItem.placeholder"
-                :clearable="formItem.clearable"
-                filterable
-                @change="controlChange(formItem['model'])">
-                <el-option
-                  :label="option.label"
-                  :value="option.value"
-                  v-for="(option, index) in formItem.options"
-                  :key="option.key || index"
-                ></el-option>
-              </el-select>
-            </el-col>
-
-            <!-- type === datePicker -->
-            <el-col :span="formItem.colSpan || defaultColSpan" v-else-if="formItem.controlName === 'datePicker'">
-              <el-date-picker
-                v-model="formModel[formItem['model']]"
-                :type="formItem.type"
-                :placeholder="formItem.placeholder"
-                :clearable="formItem.clearable"
-                :picker-options="formItem.pickerOptions || {}"
-                :range-separator="formItem.rangeSeparator || '-'"
-                :start-placeholder="formItem.startPlaceholder || ''"
-                :end-placeholder="formItem.endPlaceholder || ''"
-                :format="formItem.format || 'yyyy-MM-dd'"
-                @change="controlChange(formItem['model'])">
-              </el-date-picker>
-            </el-col>
-
-            <!-- type === switch -->
-            <el-col :span="formItem.colSpan || defaultColSpan" v-else-if="formItem.controlName === 'switch'">
-              <el-switch
-                v-model="formModel[formItem['model']]"
-                :active-text="formItem.activeText || ''"
-                :inactive-text="formItem.inactiveText || ''"
-                :active-color="formItem.activeColor || '#409EFF'"
-                :inactive-color="formItem.inactiveColor || '#C0CCDA'"
-                :disabled="formItem.disabled || false"
-                @change="controlChange(formItem['model'])"
-              >
-              </el-switch>
-            </el-col>
-
-            <!-- type === checkbox-group -->
-            <el-col :span="formItem.colSpan || defaultColSpan" v-else-if="formItem.controlName === 'checkbox-group'">
-              <el-checkbox-group
-                  v-model="formModel[formItem['model']]"
-                  @change="controlChange(formItem['model'])"
+    <div class="sd-search-form">
+        <el-form
+            ref="form"
+            :model="formModel"
+            :inline="false"
+            :label-width="formLayouts.labelWidth"
+        >
+            <el-row :gutter="formLayouts.gutter">
+                <el-col
+                    :sm="12"
+                    :md="8"
+                    :lg="6"
+                    v-for="(formItem, index) in formItems"
+                    :key="formItem.key || index"
                 >
-                <el-checkbox
-                  v-for="(checkbox, index) in formItem.checkboxes"
-                  :label="checkbox.label"
-                  :key="checkbox.key || index"
-                ></el-checkbox>
-              </el-checkbox-group>
-            </el-col>
-            </el-form-item>
-          </transition>
-        </el-col>
-      </el-row>
+                    <transition name="el-zoom-in-top">
+                        <el-form-item
+                            :label="formItem.label"
+                            :prop="formItem['model']"
+                            v-show="!formItem.hidden"
+                        >
+                            <!-- type === input-->
+                            <el-col
+                                :span="formItem.colSpan || defaultColSpan"
+                                v-if="formItem.controlName === 'input'"
+                            >
+                                <el-input
+                                    v-model="formModel[formItem['model']]"
+                                    :placeholder="formItem.placeholder"
+                                    :clearable="formItem.clearable"
+                                    :type="formItem.type || 'text'"
+                                    @change="controlChange(formItem['model'])"
+                                >
+                                </el-input>
+                            </el-col>
 
-      <el-row :gutter="formLayouts.gutter">
-        <div class="sd-search-form-operation">
-          <div class="sd-search-form-btn" style="flex: 1">
-            <span class="search_button" @click="reset">重置</span>
-            <span class="search_button" style="margin-left:10px" @click="search">查询</span>
-          </div>
-          <div class="left_span sd-search-form-expand" style="flex: 0 0 50px" @click="toggleSearch" v-if="showHiddenParts">
-            <div v-if="toggleSearchParts === 'up'"><i class="upArrow"></i><br />收起</div>
-            <div v-else-if="toggleSearchParts === 'down'"><i class="downArrow"></i><br />展开</div>
-          </div>
-        </div>
-      </el-row>
-    </el-form>
-  </div>
+                            <!-- type === select -->
+                            <el-col
+                                :span="formItem.colSpan || defaultColSpan"
+                                v-else-if="formItem.controlName === 'select'"
+                            >
+                                <el-select
+                                    v-model="formModel[formItem['model']]"
+                                    :placeholder="formItem.placeholder"
+                                    :clearable="formItem.clearable"
+                                    filterable
+                                    @change="controlChange(formItem['model'])"
+                                >
+                                    <el-option
+                                        :label="option.label"
+                                        :value="option.value"
+                                        v-for="(option,
+                                        index) in formItem.options"
+                                        :key="option.key || index"
+                                    ></el-option>
+                                </el-select>
+                            </el-col>
+
+                            <!-- type === datePicker -->
+                            <el-col
+                                :span="formItem.colSpan || defaultColSpan"
+                                v-else-if="
+                                    formItem.controlName === 'datePicker'
+                                "
+                            >
+                                <el-date-picker
+                                    v-model="formModel[formItem['model']]"
+                                    :type="formItem.type"
+                                    :placeholder="formItem.placeholder"
+                                    :clearable="formItem.clearable"
+                                    :picker-options="
+                                        formItem.pickerOptions || {}
+                                    "
+                                    :range-separator="
+                                        formItem.rangeSeparator || '-'
+                                    "
+                                    :start-placeholder="
+                                        formItem.startPlaceholder || ''
+                                    "
+                                    :end-placeholder="
+                                        formItem.endPlaceholder || ''
+                                    "
+                                    :format="formItem.format || 'yyyy-MM-dd'"
+                                    @change="controlChange(formItem['model'])"
+                                >
+                                </el-date-picker>
+                            </el-col>
+
+                            <!-- type === switch -->
+                            <el-col
+                                :span="formItem.colSpan || defaultColSpan"
+                                v-else-if="formItem.controlName === 'switch'"
+                            >
+                                <el-switch
+                                    v-model="formModel[formItem['model']]"
+                                    :active-text="formItem.activeText || ''"
+                                    :inactive-text="formItem.inactiveText || ''"
+                                    :active-color="
+                                        formItem.activeColor || '#409EFF'
+                                    "
+                                    :inactive-color="
+                                        formItem.inactiveColor || '#C0CCDA'
+                                    "
+                                    :disabled="formItem.disabled || false"
+                                    @change="controlChange(formItem['model'])"
+                                >
+                                </el-switch>
+                            </el-col>
+
+                            <!-- type === checkbox-group -->
+                            <el-col
+                                :span="formItem.colSpan || defaultColSpan"
+                                v-else-if="
+                                    formItem.controlName === 'checkbox-group'
+                                "
+                            >
+                                <el-checkbox-group
+                                    v-model="formModel[formItem['model']]"
+                                    @change="controlChange(formItem['model'])"
+                                >
+                                    <el-checkbox
+                                        v-for="(checkbox,
+                                        index) in formItem.checkboxes"
+                                        :label="checkbox.label"
+                                        :key="checkbox.key || index"
+                                    ></el-checkbox>
+                                </el-checkbox-group>
+                            </el-col>
+                        </el-form-item>
+                    </transition>
+                </el-col>
+            </el-row>
+
+            <el-row :gutter="formLayouts.gutter">
+                <div class="sd-search-form-operation">
+                    <div class="sd-search-form-btn" style="flex: 1;">
+                        <span class="search_button" @click="reset">重置</span>
+                        <span
+                            class="search_button"
+                            style="margin-left: 10px;"
+                            @click="search"
+                            >查询</span
+                        >
+                    </div>
+                    <div
+                        class="left_span sd-search-form-expand"
+                        style="flex: 0 0 50px;"
+                        @click="toggleSearch"
+                        v-if="showHiddenParts"
+                    >
+                        <div v-if="toggleSearchParts === 'up'">
+                            <i class="upArrow"></i><br />收起
+                        </div>
+                        <div v-else-if="toggleSearchParts === 'down'">
+                            <i class="downArrow"></i><br />展开
+                        </div>
+                    </div>
+                </div>
+            </el-row>
+        </el-form>
+    </div>
 </template>
 
 <script>
@@ -161,40 +214,40 @@ export default {
         /** form表单配置 */
         formItems: {
             default: [],
-            type: Array
+            type: Array,
         },
         formModel: {
             default: {},
-            type: Object
+            type: Object,
         },
         /** 布局配置 */
         formLayouts: {
             default: {
                 gutter: 20,
                 colSpan: 6,
-                labelWidth: "90px"
+                labelWidth: "90px",
             },
-            type: Object
+            type: Object,
         },
         /** callBack */
         eventFun: {
             default: null,
-            type: Function
+            type: Function,
         },
         inline: {
             default: true,
-            type: Boolean
-        }
+            type: Boolean,
+        },
     },
     components: {
-        CollapseTransition
+        CollapseTransition,
     },
     data() {
         return {
             defaultColSpan: 20,
             showHiddenParts: false,
             /** 该属性存在2个值： 'up'(收起) 'down'(展开) */
-            toggleSearchParts: ""
+            toggleSearchParts: "",
         };
     },
     methods: {
@@ -206,7 +259,7 @@ export default {
             isFunction(this.eventFun) &&
                 this.eventFun({
                     action: "formControlChange",
-                    modelName: model
+                    modelName: model,
                 });
         },
         /**
@@ -236,7 +289,7 @@ export default {
                 throw Error("formItems应该为数组类型");
             }
 
-            this["showHiddenParts"] = source.some(item =>
+            this["showHiddenParts"] = source.some((item) =>
                 item.hasOwnProperty("hidden")
             );
             this["toggleSearchParts"] =
@@ -255,16 +308,14 @@ export default {
                     item["hidden"] = this.toggleSearchParts === "down";
                 }
             });
-        }
+        },
     },
     created() {
         this.initSearchPartsValue({ source: this.formItems });
-    }
+    },
 };
 </script>
 
 <style lang="less" scoped>
 @import url("./index.less");
 </style>
-
--->
