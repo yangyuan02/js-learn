67b509e55345ad081ed7aea36fd2b0231ff97151
yangyuan <1367511704@qq.com>
2020-06-12 23:47:34 +0800

    feat(mapGetters的使用)

--- vue/vue通信/vuex使用/test.vue
+++ vue/vue通信/vuex使用/test.vue
@@ -12,10 +12,15 @@ export default {
         // name() { // 一.直接使用$store进行绑定到computed上
         //     return this.$store.state.UserStore.name; // vuex具有响应式所以一般放在computed中
         // },
-        ...mapState({
-            // 二.mapState返回的是对象所以用扩展符号 // UserStore用不用取决于 store.js ==> modules 是否模块化
-            name: (state) => state.UserStore.name,
-            age: (state) => state.UserStore.age,
+        // ...mapState({
+        //     // 二.mapState返回的是对象所以用扩展符号 // UserStore用不用取决于 store.js ==> modules 是否模块化
+        //     name: (state) => state.UserStore.name,
+        //     age: (state) => state.UserStore.age,
+        // }),
+
+        ...mapGetters(["getUserNmae"]), // 模板中{{getUserNmae}}  // user.js ===> getters ===> getUserNmae
+        ...mapGetters({
+            aliasGetUserNmae: "getUserNmae", // 新的别名 mapGetters 没有受到 store.js ===> modules的影响可以不加UserStore能够方法的到
         }),
     },
     mounted() {
--- vue/vue通信/vuex使用/user.js
+++ vue/vue通信/vuex使用/user.js
@@ -1,9 +1,19 @@
 const state = {
     name: "",
+    todo: [],
 };
 
 const getters = {
-    getUserNmae: (state) => state.name,
+    // getters一般是用于 store 中的 state 中派生出一些状态 例如doneTodos
+    getUserNmae: (state) => state.name, // this.$store.getters.getUserNmae ==> getter 在通过属性访问时是作为 Vue 的响应式系统的一部分缓存其中的
+    doneTodos: (state) => state.todo.filter((item) => item.done),
+    getTodoById: (state) => (id) => {
+        // 通过返回一个方法进行传参获取数据/ 在computed使用 ==> this.$store.getters.getTodoById(2)
+        return state.todos.find((todo) => todo.id === id);
+    },
+    doneTodosCount: (state, getters) => {
+        return getters.doneTodos.length;
+    },
 };
 
 const mutations = {
