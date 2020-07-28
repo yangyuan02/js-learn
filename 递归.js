d220ff2d65b638913d7e97003a9dd1c4a10f81f4
yangyuan <1367511704@qq.com>
2020-07-28 23:04:57 +0800

    fix(递归一些笔记)

--- 递归.js
+++ 递归.js
@@ -2,22 +2,47 @@
 var list = [
     {
         age: 12,
+        id: "0",
         son: [
             {
                 age: 13,
-                son: [{ age: 14 }],
+                id: "2",
+                son: [
+                    { age: 14, id: "0" },
+                    // {
+                    //     age: 15,
+                    //     id: "0",
+                    //     son: [
+                    //         {
+                    //             age: 16,
+                    //             id:'1'
+                    //         },
+                    //     ],
+                    // },
+                ],
             },
         ],
     },
 ];
 // var result = []
 function test(list) {
+    const newList = list;
     if (Array.isArray(list) && list.length > 0) {
         list.forEach((item, i) => {
-            item.name = "yangyuan";
+            if (item.id === "0" && !item.son) {
+                list.splice(i, 1);
+            } else if (item.id === "0" && item.son) {
+                item.disabled = true;
+            } else {
+                item.disabled = false;
+            }
+            if (item.son && item.son.length === 0) {
+                delete item["son"];
+            }
             test(item.son);
         });
     }
+    return newList;
 }
 test(list);
 // console.log(result)
