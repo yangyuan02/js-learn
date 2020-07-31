9b4b2b5af8793ad1b122dca0055e8461809d98c0
yangyuan <1367511704@qq.com>
2020-07-31 17:55:37 +0800

    fix(算法更新)

---
+++ 简单算法/遍历对象查找指定值并返回路径.js
@@ -0,0 +1,25 @@
+let obj = {
+    key1: "str1",
+    key2: {
+        key3: "str3",
+    },
+    key4: {
+        key5: {
+            key6: "str6",
+            key7: "str7",
+        },
+        key8: "str8",
+    },
+    key9: "str9",
+};
+
+function search(object, value) {
+    for (var key in object) {
+        if (object[key] === value) return [key]; // 找到了
+        if (typeof object[key] === "object") {
+            // 是对象继续递归
+            var temp = search(object[key], value);
+            if (temp) return [key, temp].flat();
+        }
+    }
+}
