d79f630c376e7513b5bac753e87bef4454d3618b
yangyuan <1367511704@qq.com>
2021-06-22 02:25:41 +0800

    递归执行

--- 数据结构/递归/递归执行的顺序.js
+++ 数据结构/递归/递归执行的顺序.js
@@ -66,3 +66,69 @@ fe: 2
 fe: 1
 
 // 总结：始终记住js是 “解释执行”的这个特点，执行是从上到下的。在没有异步的情况下，js代码是不能跳过前面的代码 去执行后面的代码的。
+var tree = {
+    name: 'a',
+    checked: true,
+    children: [{
+        name: 'b',
+        checked: false,
+        children: [{
+            name: 'c',
+            checked: true,
+            children: [{
+                name: 'd',
+                checked: true
+            }]
+        }]
+    }]
+}
+const result = [];
+function test (data, aaaa) {
+    if (!data.children) {
+        return true;
+    }
+    console.log("a", data)
+    let tmp = {};
+    const temp = test(data.children[0], tmp)
+
+    if (temp) {
+        if (data.checked) {
+            const { children, ...outer } = data;
+            tmp = outer
+        }
+        aaaa.children = [];
+        aaaa.children.push(tmp)
+        console.log("b", data)
+        console.log(aaaa)
+        return true;
+        // const { children, ...outer } = data;
+        // result.push(outer)
+    }
+    console.log(aaaa)
+    return false
+}
+test(tree, {})
+let tree = {}
+while (result.length) {
+    const data = result.shift()
+    tree = data;
+    data.children = [...data]
+}
+
+
+const aa = {
+    name: 'c',
+    checked: true
+}
+
+const b = {
+    name: 'b',
+    checked: true,
+    children: [aa]
+}
+
+const cc = {
+    name: 'cc',
+    checked: true,
+    children: [bb]
+}
