5431c866223d2c1255edc9f127f17a1fe4779d04
yangyuan <1367511704@qq.com>
2021-06-25 01:33:25 +0800

    shifit

--- 数据结构/数组/模拟数组一些方法.ts
+++ 数据结构/数组/模拟数组一些方法.ts
@@ -6,25 +6,62 @@
  *  numbers[numbers.length-1] // 4
  */
 
-/** 单文件扩展原型 */ 
+/**
+ * 单文件扩展原型
+ */
 declare interface Array<T> {
     insterFirstPosition<T>(value:T):number
+    removeFisrtPosition<T>():T
+}
+/**
+ * 抽象类
+ */
+// abstract class ArrayUitlsShape extends Array {
+//     abstract insterFirstPosition<T>(value:T):number
+// }
+
+interface ArrayUitlsShape{
+    insterFirstPosition<T>(value:T):number
+    removeFisrtPosition<T>():T
 }
 
-interface ArrayUitlsShape {
-    insterFirstPosition<T>(value:T):number
-}
+/**
+ * 工具方法实现
+ */
 class ArrayUitls extends Array implements ArrayUitlsShape{
     /**
      * @description 模拟unshift
      * @param value 
-     * @returns 
+     * @returns 新数组的长度
      */
-     public insterFirstPosition <T>(value:T):number {
+    public insterFirstPosition <T>(value:T):number {
         for (let i = this.length; i >=0; i--) {
             this[i] = this[i-1]
         }
         this[0] = value;
         return this.length;
     }
+    private reIndex<T>(array:T[]){
+        const newArray = [];
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] !== undefined) {
+                newArray.push(array[i])
+            }
+        }
+        return newArray
+    }
+    /**
+     * @description 模拟shift
+     * @returns 被删除的值
+     */
+    public removeFisrtPosition<T>():T | undefined{
+        const oldArray = JSON.parse(JSON.stringify(this))
+        for (let i = 0; i < this.length; i++) {
+            this[i] = this[i+1]
+        }
+        this.reIndex(this)
+        return oldArray[0]
+    }
 }
+
+const a =['1', 2].removeFisrtPosition()
