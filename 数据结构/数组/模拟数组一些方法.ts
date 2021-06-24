cfaad543fbe13ac2b683de298fe1537279bf4045
yangyuan <1367511704@qq.com>
2021-06-25 00:39:29 +0800

    模拟数组方法

--- 数据结构/数组/模拟数组一些方法.ts
+++ 数据结构/数组/模拟数组一些方法.ts
@@ -6,24 +6,25 @@
  *  numbers[numbers.length-1] // 4
  */
 
-
 /** 单文件扩展原型 */ 
 declare interface Array<T> {
-    insterFirstPosition<T>(value:T):T
+    insterFirstPosition<T>(value:T):number
 }
 
-
-class ArrayUitls extends Array {
+interface ArrayUitlsShape {
+    insterFirstPosition<T>(value:T):number
+}
+class ArrayUitls extends Array implements ArrayUitlsShape{
     /**
      * @description 模拟unshift
      * @param value 
      * @returns 
      */
-     public insterFirstPosition <T>(value:T):T {
+     public insterFirstPosition <T>(value:T):number {
         for (let i = this.length; i >=0; i--) {
             this[i] = this[i-1]
         }
         this[0] = value;
-        return value
+        return this.length;
     }
 }
